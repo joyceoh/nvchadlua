6dafd274fc9cce77feb044b66ee6c17cf6eb7347
joyceoh <joyce.oh@pendo.io>
2025-05-22 11:22:05 -0400

    added caps lock mapping probably will change... is scuffed

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map({'n', 'v', 'i'}, '<CapsLock>', '<C-', { remap = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- map key for undo tree
